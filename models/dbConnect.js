823de42aab9b52747cc18054f179cea4a1a4f915
Benjamin Faruna <benjaminfaruna@gmail.com>
2023-12-16 18:44:33 +0100

    updated db url format

--- models/dbConnect.js
+++ models/dbConnect.js
@@ -1,13 +1,12 @@
 /* eslint-disable no-undef */
 const mongoose = require("mongoose");
 
-const MONGO_DB_URL = process.env.MONGO_DB_URL || "mongodb://localhost:27017";
-const DB_NAME = process.env.DB_NAME || "quiz_db";
+const MONGO_DB_URL = process.env.MONGO_DB_URL || "mongodb://localhost:27017/quiz_db";
 
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect(MONGO_DB_URL + '/' + DB_NAME);
+  await mongoose.connect(MONGO_DB_URL);
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
